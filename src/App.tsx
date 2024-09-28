aa0d7f818a68cfae87a6a7b5fd5259ecc68ab0c8
James Robertson <jamespr.0312@gmail.com>
2024-09-27 20:58:08 -0500

    AppShell added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import { Button, Text, TextInput, Title } from "@mantine/core";
+import { AppShell, Button, Text, TextInput, Title } from "@mantine/core";
 
 import classes from "./App.module.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
+  const [greeting, setGreeting] = useState("");
   const [name, setName] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreeting(await invoke("greet", { name }));
   }
 
   return(
     <div className={classes.appMain}>
-      <Title className={classes.title} ta="center">Eager Ermine</Title>
-      <form className={classes.inputs}
-        onSubmit={(e) => {e.preventDefault(); greet(); }}
+      <AppShell
+        header={{height: 50}}
       >
-        <TextInput onChange={(e) => setName(e.currentTarget.value)} id="greet-input" placeholder="Enter your name"/>
-        <Button type="submit" >Say hi!</Button>
-      </form>
-      <Text ta='center'>{greetMsg}</Text>
+        <AppShell.Header>
+          <Text>Appbar!</Text>
+        </AppShell.Header>
+        <AppShell.Main>
+          <Title className={classes.title} ta="center">Eager Ermine</Title>
+          <form className={classes.inputs}
+            onSubmit={(e) => {e.preventDefault(); greet(); }}
+          >
+            <TextInput onChange={(e) => setName(e.currentTarget.value)} id="greet-input" placeholder="Enter your name"/>
+            <Button type="submit" >Say hi!</Button>
+          </form>
+          <Text ta='center'>{greeting}</Text>
+        </AppShell.Main>
+      </AppShell>
     </div>
   )
 }
